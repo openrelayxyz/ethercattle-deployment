b174154fa886e76efdfc13d2452f37b44aaed510
Austin Roberts <code@ausiv.com>
2020-01-15 10:49:23 -0600

    Increase snapshot frequency
    This updates snapshots to take place every 6 hours, instead of daily.
    
    It also updates the snapshot garbage collector to retain:
    
    * 2 most recent snapshots from today
    * 1 Most recent snapshot from yesterday
    * 2 additional days of snapshots

--- devops/gcSnapshot.py
+++ devops/gcSnapshot.py
@@ -1,8 +1,10 @@
 import boto3
 import os
+import datetime
 
 ec2 = boto3.client('ec2')
-keep_count = int(os.environ.get("KEEP_COUNT", 4))
+keep_count = int(os.environ.get("KEEP_COUNT", 2))
+today_keep_count = int(os.environ.get("TODAY_KEEP_COUNT", 2))
 
 
 def handler(event, context):
@@ -12,11 +14,18 @@ def handler(event, context):
             "Values": [os.environ.get("CLUSTER_ID")]
         }]
     )
-    stale_snapshots = sorted(
+    sorted_snaps = sorted(
         [i for i in rsp["Snapshots"] if i["Progress"] == "100%"],
         key=lambda i: i["StartTime"]
-    )[:-keep_count]
-    for snapshot in stale_snapshots:
+    )
+    today = datetime.datetime.combine(
+            datetime.date.today(), datetime.datetime.min.time()
+        ).replace(tzinfo=sorted_snaps[0]["StartTime"].tzinfo)
+    yesterday = today - datetime.timedelta(days=1)
+    today_snaps = [i for i in sorted_snaps if i["StartTime"] >= today]
+    yesterday_snaps = [i for i in sorted_snaps if yesterday <= i["StartTime"] < today]
+    older_snaps = [i for i in sorted_snaps if i["StartTime"] < yesterday]
+    for snapshot in today_snaps[:-today_keep_count] + older_snaps[:-keep_count] + yesterday_snaps[:-1]:
         ec2.delete_snapshot(
             SnapshotId=snapshot["SnapshotId"]
         )
