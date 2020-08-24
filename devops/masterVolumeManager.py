2c16faff72bf4a1b5e2a60a14423f8664f468d01
Austin Roberts <code@ausiv.com>
2020-08-24 11:23:30 -0500

    Move master disk resizing to lambda function
    To better manage permissions and better handle issues with disks that
    are already being modified, we're moving disk resizing to a lambda
    function instead of doing it withing the snapshotter.

---
+++ devops/masterVolumeManager.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+client = boto3.client("ec2")
+
+def sizeHandler(event, context):
+    target_size = int(os.environ.get("VOLUME_SIZE"))
+    volumes = client.describe_volumes(Filters=[
+        {"Name": "tag:Name", "Values": ["%s-Master" % os.environ.get("KAFKA_TOPIC")]},
+        {"Name":"attachment.device", "Values": ["/dev/sdf"]}
+    ])["Volumes"]
+    for volume in volumes:
+        if volume["Size"] < target_size:
+            client.modify_volume(
+                VolumeId: volume["VolumeId"],
+                Size: target_size,
+            )
